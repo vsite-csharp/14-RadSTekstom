76e51b8c40417c6a32fbffce3a768fb9f39ababd
Kata Pogacic <KPogacic@dspace.hr>
2022-01-13 21:07:28 +0100

    Unicode

--- Unicode/Unicode.cs
+++ Unicode/Unicode.cs
@@ -4,12 +4,12 @@ namespace Vsite.CSharp.RadSTekstom
 {
     class Unicode
     {
-        // TODO:000 Pogledati imena donjih varijabli te sadržaje tekstova
-        // TODO:001 Pokrenuti program i provjeriti ispis
+        // 000 Pogledati imena donjih varijabli te sadržaje tekstova
+        // 001 Pokrenuti program i provjeriti ispis
         static void Main(string[] args)
         {
-            // TODO:002 Otkomentirati donju naredbu i ponovno pokrenuti program te provjeriti ispis
-            //Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // 002 Otkomentirati donju naredbu i ponovno pokrenuti program te provjeriti ispis
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
             // latinica
             string varijabla = "Đakovački Božić u šumi";
