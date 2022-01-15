39a7c196af914b6449d4685444f10fb3b3a1cd9b
Julijan Šribar <julijan.sribar@gmail.com>
2022-01-15 22:01:02 +0100

    Modify projects for string immutability and encoding, add projects for StringBuilder and Base-64 encoding

---
+++ Testovi/KodiranjeBase64.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+
+namespace Vsite.CSharp.RadSTekstom.Testovi
+{
+    [TestClass]
+    public class KodiranjeBase64 : ConsoleTest
+    {
+        private void PreskočiNaslov()
+        {
+            string učitano = cw.GetString();
+            while (!učitano.StartsWith("*** "))
+            {
+                učitano = cw.GetString();
+            }
+        }
+
+        private string UčitajBlok()
+        {
+            System.Text.StringBuilder tekst = new System.Text.StringBuilder(); 
+            string učitano;
+            while ((učitano = cw.GetString()).Length > 0)
+            {
+                tekst.Append(učitano);
+            }
+            return tekst.ToString();
+        }
+
+        [TestMethod]
+        public void PovratnaKonverzijaVraćaIzvorniNizBajtova()
+        {
+            Vsite.CSharp.RadSTekstom.KodiranjeBase64.NapraviBase64Konverzije();
+            Assert.IsTrue(cw.Count > 45);
+
+            PreskočiNaslov();
+            var početni = UčitajBlok();
+
+            PreskočiNaslov();
+            var kodirani = UčitajBlok();
+
+            PreskočiNaslov();
+            var dekodirani = UčitajBlok();
+
+            Assert.AreEqual(početni, dekodirani);
+        }
+    }
+}
