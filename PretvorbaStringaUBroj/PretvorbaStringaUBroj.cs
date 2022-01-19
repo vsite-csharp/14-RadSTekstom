d0b96196ac3086774323a59c5c990cca1ec1a431
Ana Lovric <alovric@dspace.hr>
2022-01-19 13:06:24 +0100

    DZ: Resloved transforming string to number.

--- PretvorbaStringaUBroj/PretvorbaStringaUBroj.cs
+++ PretvorbaStringaUBroj/PretvorbaStringaUBroj.cs
@@ -5,25 +5,32 @@ namespace Vsite.CSharp.RadSTekstom
 {
     class PretvorbaStringaUBroj
     {
-        // TODO:040 Dodati u donju metodu poziv metode Parse koja će tekst pretvoriti u double i vratiti rezultat. Pokrenuti program i provjeriti ispise.
+        // 040 Dodati u donju metodu poziv metode Parse koja će tekst pretvoriti u double i vratiti rezultat. Pokrenuti program i provjeriti ispise.
         public static double UDouble(string tekst)
         {
-            throw new NotImplementedException();
+            return double.Parse(tekst);
         }
 
-        // TODO:041 Dodati u donju metodu poziv metode Parse koja kao drugi argument prima IFormatProvider i vratiti rezultat. Pokrenuti program i provjeriti ispise
+        // 041 Dodati u donju metodu poziv metode Parse koja kao drugi argument prima IFormatProvider i vratiti rezultat. Pokrenuti program i provjeriti ispise
         public static double UDouble(string tekst, IFormatProvider formatProvider)
         {
-            throw new NotImplementedException();
+            return double.Parse(tekst,formatProvider);
         }
 
-        // TODO:042 Dodati u donju metodu poziv metode TryParse. Ako je pretvornba uspjela, ispisati rezultat, inače ispisati poruku "Neuspješno". Pokrenuti program i provjeriti ispise
+        // 042 Dodati u donju metodu poziv metode TryParse. Ako je pretvornba uspjela, ispisati rezultat, inače ispisati poruku "Neuspješno". Pokrenuti program i provjeriti ispise
         public static void PokušajUDouble(string tekst)
         {
-            throw new NotImplementedException();
+            if (double.TryParse(tekst, out double dtekst))
+            {
+                Console.WriteLine(dtekst);
+            }
+            else
+            {
+                Console.WriteLine("Neuspješno");
+            }
         }
 
-        // TODO:043 Pokrenuti testove (4 testa u grupi "TestStringUBroj" moraju proći)
+        // 043 Pokrenuti testove (4 testa u grupi "TestStringUBroj" moraju proći)
         static void Main(string[] args)
         {
             try
