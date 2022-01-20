976b32aa20978f3e67e89181496e3b16c0a9e003
robilovric <robilovric7@gmail.com>
2022-01-20 19:41:20 +0100

    Resolve dialogization

--- LokalizacijaDijaloga/LocalizedForm.cs
+++ LokalizacijaDijaloga/LocalizedForm.cs
@@ -17,14 +17,14 @@ namespace Vsite.CSharp.RadSTekstom
             InitializeComponent();
         }
 
-        // TODO:070 Pokrenuti program. Otvoriti u File Exploreru kazalo projekta te pogledati sadržaj podkazala bin\Debug
-        // TODO:071 Omogućiti lokalizaciju dijaloga za različite jezike: otvoriti LocalizedForm u Designeru te u prozoru Properties za LocalizedForm promijeniti svojstvo "Localizable" u true
-        // TODO:072 U prozoru Properties za LocalizedForm svojstvo "Language" promijeniti "Croatian" te za svaku kontrolu (uključujući i zaglavlje glavne forme) prevesti svojstvo Text na hrvatski tekst:
+        //:070 Pokrenuti program. Otvoriti u File Exploreru kazalo projekta te pogledati sadržaj podkazala bin\Debug
+        //:071 Omogućiti lokalizaciju dijaloga za različite jezike: otvoriti LocalizedForm u Designeru te u prozoru Properties za LocalizedForm promijeniti svojstvo "Localizable" u true
+        //:072 U prozoru Properties za LocalizedForm svojstvo "Language" promijeniti "Croatian" te za svaku kontrolu (uključujući i zaglavlje glavne forme) prevesti svojstvo Text na hrvatski tekst:
         //  - naslov "Main Form" prevesti u "Glavna Forma"
         //  - "Language:" prevesti u "Jezik:"
         //  - "Hello!" prevesti u "Dobar dan!"
-        // TODO:073 Proširiti čvor LocalizedForm u SolutionExploreru i provjeriti koje su datoteke dodane te pogledati njihov sadržaj
-        // TODO:074 Provjeriti što se događa s tekstovima u LocalizedForm kada se svojstvo Language vrati na "Default"
+        //:073 Proširiti čvor LocalizedForm u SolutionExploreru i provjeriti koje su datoteke dodane te pogledati njihov sadržaj
+        //:074 Provjeriti što se događa s tekstovima u LocalizedForm kada se svojstvo Language vrati na "Default"
         // TODO:075 Buildati program i provjeriti kamo su se prevedeni resursi pohranili (tj. pogledati sadržaj potkazala bin\Debug)
         // TODO:076 Pokrenuti program i provjeriti mijenja li se tekst kontrola pri promjeni izbora u padajućoj listi na formi
         // TODO:077 U prozoru Properties za LocalizedForm promijeniti language na "German" te prevesti tekstove:
@@ -42,6 +42,11 @@ namespace Vsite.CSharp.RadSTekstom
                 Text = resources.GetString("$this.Text");
             foreach (Control control in Controls)
                 resources.ApplyResources(control, control.Name);
+        }
+
+        private void LocalizedForm_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
