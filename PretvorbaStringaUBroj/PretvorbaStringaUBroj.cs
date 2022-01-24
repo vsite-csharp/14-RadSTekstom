4efcccf9113122e7e213383323ae59e02d9a7675
Ida Midžić <idmidzic@vsite.hr>
2022-01-24 23:17:09 +0100

    Riješeno string u broj

--- PretvorbaStringaUBroj/PretvorbaStringaUBroj.cs
+++ PretvorbaStringaUBroj/PretvorbaStringaUBroj.cs
@@ -8,22 +8,27 @@ namespace Vsite.CSharp.RadSTekstom
         // :040 Dodati u donju metodu poziv metode Parse koja će tekst pretvoriti u double i vratiti rezultat. Pokrenuti program i provjeriti ispise.
         public static double UDouble(string tekst)
         {
-            return Double.Parse(tekst);
+            return double.Parse(tekst);
         }
 
-        // TODO:041 Dodati u donju metodu poziv metode Parse koja kao drugi argument prima IFormatProvider i vratiti rezultat. Pokrenuti program i provjeriti ispise
+        // :041 Dodati u donju metodu poziv metode Parse koja kao drugi argument prima IFormatProvider i vratiti rezultat. Pokrenuti program i provjeriti ispise
         public static double UDouble(string tekst, IFormatProvider formatProvider)
         {
-            throw new NotImplementedException();
+            return double.Parse(tekst, formatProvider);
         }
 
-        // TODO:042 Dodati u donju metodu poziv metode TryParse. Ako je pretvornba uspjela, ispisati rezultat, inače ispisati poruku "Neuspješno". Pokrenuti program i provjeriti ispise
+        // :042 Dodati u donju metodu poziv metode TryParse. Ako je pretvornba uspjela, ispisati rezultat, inače ispisati poruku "Neuspješno". Pokrenuti program i provjeriti ispise
         public static void PokušajUDouble(string tekst)
         {
-            throw new NotImplementedException();
+            if (double.TryParse(tekst, out double rezultat))
+                Console.WriteLine(rezultat);
+            else
+            {
+                Console.WriteLine("Neuspješno");
+            }
         }
 
-        // TODO:043 Pokrenuti testove (4 testa u grupi "TestStringUBroj" moraju proći)
+        // :043 Pokrenuti testove (4 testa u grupi "TestStringUBroj" moraju proći)
         static void Main(string[] args)
         {
             try
